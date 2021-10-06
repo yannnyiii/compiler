49aba060f7cd589852e00da0ed1b4eae96c36c07
unknown <1637510168@qq.com>
2021-10-06 16:55:30 +0800

    wow

---
+++ src/deal.java
@@ -0,0 +1,132 @@
+import java.util.*;
+public class deal {
+	public static void compile(char temchar) {
+		if(Character.isDigit(temchar)) {
+			compilenum(temchar);
+			return;
+		}
+		if(Character.isLowerCase(temchar)) {
+			compilelow(temchar);
+			return;
+		}
+		if(Character.isUpperCase(temchar)) {
+			compileup(temchar);
+			return;
+		}
+		if(temchar==';') {
+			System.out.println("Semicolon");
+			return;
+		}
+		if(temchar=='(') {
+			System.out.println("LPar");
+			return;
+		}
+		if(temchar==')') {
+			System.out.println("RPar");
+			return;
+		}
+		if(temchar=='{') {
+			System.out.println("LBrace");
+			return;
+		}
+		if(temchar=='}') {
+			System.out.println("RBrace");
+			return;
+		}
+		if(temchar=='+') {
+			System.out.println("Plus");
+			return;
+		}
+		if(temchar=='*') {
+			System.out.println("Mult");
+			return;
+		}
+		if(temchar=='/') {
+			System.out.println("Div");
+			return;
+		}
+		if(temchar=='<') {
+			System.out.println("Lt");
+			return;
+		}
+		if(temchar=='>') {
+			System.out.println("Gt");
+			return;
+		}
+		if(temchar==' '||temchar=='\r'||temchar=='\n') {
+			return;
+		}
+		if(temchar=='=') {
+			test.flag++;
+			if(test.flag < test.length&&test.flag=='=') {
+				System.out.println("Eq");
+			}
+			else {
+				System.out.println("Assign");
+			}
+			return;
+		}
+		System.out.println("Err");
+		test.esc = true;
+		return;
+	}
+	public static void compilenum(char temchar){
+		test.flag++;
+		test.num = temchar-'0';
+		while((test.flag < test.length)&&Character.isDigit(test.temString.charAt(test.flag))) {
+			test.num = test.num*10 + test.temString.charAt(test.flag)-'0';
+			test.flag++;
+		}
+		System.out.println("Number("+test.num+")");
+		test.flag--;
+	}
+	public static void compilelow(char temchar){
+		test.flag++;
+		test.alphString = String.valueOf(temchar);
+		while((test.flag < test.length)&&(Character.isLowerCase(test.temString.charAt(test.flag))||Character.isUpperCase(test.temString.charAt(test.flag))||Character.isDigit(test.temString.charAt(test.flag))||(test.temString.charAt(test.flag)=='_'))) {
+			test.alphString = test.alphString+test.temString.charAt(test.flag);
+			test.flag++;
+		}
+		if(!iskey(test.alphString))  {
+			System.out.println("Ident("+test.alphString+")");	
+		}
+		test.flag--;
+	}
+	public static void compileup(char temchar){
+		test.flag++;
+		test.alphString = String.valueOf(temchar);
+		while((test.flag < test.length)&&(Character.isLowerCase(test.temString.charAt(test.flag))||Character.isUpperCase(test.temString.charAt(test.flag))||Character.isDigit(test.temString.charAt(test.flag))||(test.temString.charAt(test.flag)=='_'))) {
+			test.alphString = test.alphString+test.temString.charAt(test.flag);
+			test.flag++;
+		}
+		System.out.println("Ident("+test.alphString+")");	
+		test.flag--;
+	}
+	public static boolean iskey(String temString) {
+		if(temString.equals("if")) {
+			System.out.println("If");
+			return true;
+		}
+		if(temString.equals("else")) {
+			System.out.println("Else");
+			return true;
+		}
+		if(temString.equals("while")) {
+			System.out.println("While");
+			return true;
+		}
+		if(temString.equals("break")) {
+			System.out.println("Break");
+			return true;
+		}
+		if(temString.equals("continue")) {
+			System.out.println("Continue");
+			return true;
+		}
+		if(temString.equals("return")) {
+			System.out.println("Return");
+			return true;
+		}
+		return false;
+	}
+}
--- src/test.java
+++ src/test.java
@@ -1,10 +1,22 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class test {
+	public static int flag = 0;
+	public static long num = 0;
+	public static String temString;
+	public static int length;
+	public static boolean esc = false;
+	public static String alphString;
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		System.out.println(a+b);
+		Scanner compile1 = new Scanner(System.in);
+		while (compile1.hasNext()) {
+			temString = compile1.nextLine();
+			length = temString.length();
+			for(flag = 0;flag< length;flag++) {
+				if(esc) {
+					return;
+				}
+				deal.compile(temString.charAt(flag));
+			}
+		}
 	}
 }
