ff91ee6dd186ba9a49060594e49ce09b0664aabe
unknown <1637510168@qq.com>
2021-11-05 19:08:35 +0800

    d

--- src/test.java
+++ src/test.java
@@ -10,43 +10,39 @@ public class test {
 	public static void main(String[] args) {
 		Scanner compile1 = new Scanner(System.in);
 		while (compile1.hasNextLine()) {
-			line++;
-			temString = compile1.nextLine();
-//			if(temString.length()==0||temString == null) {
-//				return;
+			System.out.println(compile1.nextLine());
+		}
+//		while (compile1.hasNextLine()) {
+//			line++;
+//			temString = compile1.nextLine();
+//			length = temString.length();
+//			if(temString.charAt(0)=='\r') {
+//				continue;
 //			}
-			length = temString.length();
-			if(temString.charAt(0)=='\r') {
-				continue;
-			}
-			for(flag = 0;flag< length;flag++) {
-				if(esc) {
-					System.exit(1);
-				}
-				deal.compile(temString.charAt(flag),compile1);
-//			System.out.println(deal.Wordlist.get(deal.Wordlist.size()-1).getName());
-			}
-		}
-		if(!Gram.flag) {
-			System.exit(8);
-		}
-		Gram.CompUnit();
-//		deal.printword();
-//		System.out.println(Gram.flag);
-		if(Gram.flag) {
-			System.out.println("define dso_local i32 @main(){");
-			try {
-				System.out.println("    ret i32 " + (int)Calculator.calculate(Gram.expression));
-			} catch (Exception e) {
-				System.exit(8);
-			}
-			System.out.println("}");
-			return;
-		}
-		else {
-//			System.exit(Integer.parseInt(deal.lab1));
-			System.exit(8);
-		}
+//			for(flag = 0;flag< length;flag++) {
+//				if(esc) {
+//					System.exit(1);
+//				}
+//				deal.compile(temString.charAt(flag),compile1);
+//			}
+//		}
+//		if(!Gram.flag) {
+//			System.exit(8);
+//		}
+//		Gram.CompUnit();
+//		if(Gram.flag) {
+//			System.out.println("define dso_local i32 @main(){");
+//			try {
+//				System.out.println("    ret i32 " + (int)Calculator.calculate(Gram.expression));
+//			} catch (Exception e) {
+//				System.exit(8);
+//			}
+//			System.out.println("}");
+//			return;
+//		}
+//		else {
+//			System.exit(8);
+//		}
 
 	}
 }
